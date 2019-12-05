39f96d499198f6d16bd6d62b5fbe6b449ef993ed
Jordan <jordan_j9@hotmail.com>
2019-12-05 14:34:18 -0500

    Version 4.3 05 de Diciembre 2019 14:34 Jordan

--- resources/views/welcome/index.blade.php
+++ resources/views/welcome/index.blade.php
@@ -7,6 +7,8 @@
     <title>PackPorn</title>
     <link rel="stylesheet" href="{{asset('css/main.css')}}">
     <link rel="stylesheet" href="{{asset('css/main_inicio.css')}}">
+    <link rel="stylesheet" href="{{asset('css/plugins/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('bower_components/select2/dist/css/select2.css')}}">
     <script src="https://kit.fontawesome.com/33bf1ad68d.js" crossorigin="anonymous"></script>
 </head>
 <body style="background-image: url('{{asset('img/packporn/women_tanned_ass_back_water_wet_body_wet_hair_blonde-1213897.jpg')}}');">
@@ -73,7 +75,7 @@
             <p>Tenemos esos servicios que tanto te gustan, los más ardientes y sensuales del mercado. Esté es el sitio
                 perfecto para que disfrutes,Suscríbete con nosotros y te haremos llegar todos esos contenidos de chicas
                 y/o chiscos que deseas por un buen precio.</p>
-            <a href="" class="btn-suscribirse">Suscríbirse</a>
+            <a data-toggle="modal" data-target="#modal-default" style="cursor: pointer;" class="btn-suscribirse">Suscríbirse</a>
         </section>
     </div>
 </main>
@@ -132,9 +134,72 @@
     </div>
 </footer>
 
-
+<div class="modal" tabindex="-1" id="modal-default" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Suscríbirse</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form method="POST" action="{{route('solicitud.store')}}">
+                    @csrf
+                    <div class="form-group">
+                        <label for="alias">Alias</label>
+                        <input type="text" class="form-control" id="alias" name="alias" placeholder="Enter email">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" id="email" name="email" placeholder="Enter email">
+                    </div>
+                    <div class="form-group">
+                        <label for="sexo">Sexo</label>
+                        <select class="form-control select2" id="select2" style="width: 100%;" tabindex="-1" aria-hidden="true">
+                            <option value="M">HOMBRE</option>
+                            <option value="F">MUJER</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="tipopack">Tipo Pack</label>
+                        <select class="form-control select2" name="tipopack" id="select2" style="width: 100%;" tabindex="-1" aria-hidden="true">
+                            <option value="AUDIO">AUDIO</option>
+                            <option value="VIDEO">VIDEO</option>
+                            <option value="IMAGENES">IMAGENES</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="preferencias">Preferencias</label>
+                        <select class="form-control select2" id="select2" name="preferencias" style="width: 100%;" tabindex="-1" aria-hidden="true">
+                            <option value="AUDIO">AUDIO</option>
+                            <option value="VIDEO">VIDEO</option>
+                            <option value="IMAGENES">IMAGENES</option>
+                        </select>
+                    </div>
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary">Guardar</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="{{asset('js/plugins/jquery.min.js')}}"></script>
+<script src="{{asset('js/plugins/bootstrap.min.js')}}"></script>
+<script src="{{asset('js/plugins/select2.min.js')}}"></script>
 <script src="{{asset('js/menu.js')}}"></script>
 <script src="{{asset('js/app.js')}}"></script>
-
+<script type="javascript">
+    $(document).ready(function () {
+        $("#select2").select2();
+    });
+</script>
 </body>
 </html>
